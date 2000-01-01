627393f8dadf870c03af0a7767c4245cb6d00351
Jason Kridner <jdk@ti.com>
2000-01-01 03:35:34 +0000

    blinkled: debugged for Black

--- blinkled.js
+++ blinkled.js
@@ -1,7 +1,7 @@
 // Import functions for interacting with hardware
 var b = require('bonescript');
 
-var ledPin = "P8_3";
+var ledPin = "P8_13";
 var ledPin2 = "USR3";
 
 // Initialize pins as outputs
@@ -9,7 +9,7 @@ b.pinMode(ledPin, b.OUTPUT);
 b.pinMode(ledPin2, b.OUTPUT);
 
 // Call function to toggle LEDs every 100ms
-setTimeout(toggleLEDs, 100);
+setInterval(toggleLEDs, 100);
 
 // Assume LEDs are off by default
 var state = b.LOW;
