1ba7a19718c5d21cfcf9386f2839cd2777a8db1d
Jason Kridner <jdk@ti.com>
2000-01-01 19:59:34 +0000

    test-i2c.js: Created initial test case

---
+++ test/test-i2c.js
@@ -0,0 +1,26 @@
+var b = require('bonescript');
+var port = '/dev/i2c-0'
+var address = 0x50;
+
+b.i2cOpen(port, address, {}, onI2C);
+b.i2cScan(port, onScan);
+b.i2cWriteByte(port, 0, onWriteByte);
+b.i2cReadBytes(port, null, 12, onReadBytes);
+
+function onI2C(x) {
+    if(x.event == 'data') {
+        console.log('data = ' + JSON.stringify(data));
+    }
+}
+
+function onScan(err, data) {
+    console.log('scan = ' + JSON.stringify(arguments));
+}
+
+function onWriteByte(err) {
+    console.log('writeByte = ' + JSON.stringify(err));
+}
+
+function onReadBytes(err, res) {
+    console.log('readBytes = ' + JSON.stringify(arguments));
+}
