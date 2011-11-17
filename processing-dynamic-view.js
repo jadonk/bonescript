d42911ad6e4a6f91376e596899ee94883ebd54c3
Jason Kridner <jdk@ti.com>
2011-11-17 16:46:37 +0000

    processing-dynamic-view: switched to a config.js script
    There were starting to be too many parameters to pass.

--- processing-dynamic-view.js
+++ processing-dynamic-view.js
@@ -2,29 +2,17 @@
 var bb = require('./bonescript');
 var fs = require('fs');
 var io = require('socket.io');
+var config = require('./processing-dynamic-view/config');
 
 setup = function() {
     var onconnect = function(socket) {
         console.log("New client connected");
-        var delay = 1000;
-        var scale = 1;
+        var delay = config.dynviewConfig.delay;
+        var scale = config.dynviewConfig.scale;
         
         var fileData =
             "/var/lib/cloud9/processing-dynamic-view/data";
-        var fileRangeLow =
-            "/var/lib/cloud9/processing-dynamic-view/rangeLow";
-        var fileRangeHigh =
-            "/var/lib/cloud9/processing-dynamic-view/rangeHigh";
-        var fileRate =
-            "/var/lib/cloud9/processing-dynamic-view/delay";       
-        var fileScale =
-            "/var/lib/cloud9/processing-dynamic-view/scale";
 
-        fs.readFile(fileScale, function(err, data) {
-            if(err) throw("Unable to read scale: " + err);
-            scale = data;
-        });
-        
         var readData = function(fd) {
             fs.readFile(fileData, function(err, data) {
                 if(err) throw("Unable to read data: " + err);
@@ -32,22 +20,9 @@ setup = function() {
             });
             setTimeout(readData, delay);
         };
-        
-        fs.readFile(fileRangeLow, function(err, data) {
-            if(err) throw("Unable to read rangeLow: " + err);
-            socket.emit('rangeLow', "" + data);
-        });
-            
-        fs.readFile(fileRangeHigh, function(err, data) {
-            if(err) throw("Unable to read rangeHigh: " + err);
-            socket.emit('rangeHigh', "" + data);
-        });
-            
-        fs.readFile(fileRate, function(err, data) {
-            if(err) throw("Unable to read delay: " + err);
-            delay = data;
-            setTimeout(readData, delay);
-        });
+
+        socket.emit('config', config.dynviewConfig);
+        setTimeout(readData, delay);
 
         // on message
         socket.on('message', function(data) {
---
+++ processing-dynamic-view/config.js
@@ -0,0 +1,6 @@
+exports.dynviewConfig = {
+    delay: 1000,
+    scale: 1,
+    rangeHigh: 10000,
+    rangeLow: 10,
+};
