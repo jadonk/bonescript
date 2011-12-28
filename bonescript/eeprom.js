1ac4de0c0db18aea2b4607e2ebb065590c12bf38
Jason Kridner <jdk@ti.com>
2011-12-28 16:33:54 -0500

    eeprom.js: added Buffer.readUint16BE() if missing

--- bonescript/eeprom.js
+++ bonescript/eeprom.js
@@ -2,6 +2,39 @@ var fs = require('fs');
 var buffer = require('buffer');
 var util = require('util');
 
+if(!buffer.Buffer.prototype.readUint16BE) {
+    var readUInt16 = function(buffer, offset, isBigEndian, noAssert) {
+        var val = 0;
+
+        if (!noAssert) {
+            assert.ok(typeof (isBigEndian) === 'boolean',
+                'missing or invalid endian');
+
+            assert.ok(offset !== undefined && offset !== null,
+                'missing offset');
+
+            assert.ok(offset + 1 < buffer.length,
+                'Trying to read beyond buffer length');
+        }
+
+        if (isBigEndian) {
+            val = buffer[offset] << 8;
+            val |= buffer[offset + 1];
+        } else {
+            val = buffer[offset];
+            val |= buffer[offset + 1] << 8;
+        }
+
+        return val;
+    }
+
+    buffer.Buffer.prototype.readUInt16BE = function(offset, noAssert) {
+        return readUInt16(this, offset, true, noAssert);
+    };
+}
+
+var eepromData = new buffer.Buffer(256);
+
 var readEeproms = function() {
     var data = {};
     var addresses = ['3-0054', '3-0055', '3-0056', '3-0057'];
@@ -27,7 +60,6 @@ var readEeproms = function() {
 };
 
 var fetchEepromData = function(address) {
-    var eepromData = new buffer.Buffer(256);
     try {
         var eepromFile =
             fs.openSync(
@@ -73,12 +105,12 @@ var parseCapeEeprom = function(x) {
     data.version = x.toString('ascii', 38, 42);
     data.manufacturer = x.toString('ascii', 42, 58);
     data.partNumber = x.toString('ascii', 58, 74);
-    data.numPins = parseInt(x[74]) * 16 + parseInt(x[75]);
+    data.numPins = x.readUint16BE(74);
     data.serialNumber = x.toString('ascii', 76, 88);
-    data.currentVDD_3V3EXP = parseInt(x[228]) * 16 + parseInt(x[229]);
-    data.currentVDD_5V = parseInt(x[230]) * 16 + parseInt(x[231]);
-    data.currentSYS_5V = parseInt(x[232]) * 16 + parseInt(x[233]);
-    data.DCSupplied = parseInt(x[234]) * 16 + parseInt(x[235]);
+    data.currentVDD_3V3EXP = x.readUint16BE(228);
+    data.currentVDD_5V = x.readUint16BE(230);
+    data.currentSYS_5V = x.readUint16BE(232);
+    data.DCSupplied = x.readUint16BE(234);
     return(data);
 };
 
