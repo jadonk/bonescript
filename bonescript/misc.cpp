952d0916805f245e7107b7a1d66d6e010c18e70e
Jason Kridner <jdk@ti.com>
2012-05-04 22:16:12 +0100

    pollpri basic functionality kinda working
    I ended up putting in a poll loop within a thread, rather than adding an
    event watcher to the main event loop.  This will likely be notably
    slower, but I haven't figured out how to get the watcher to behave
    properly.

--- bonescript/misc.cpp
+++ bonescript/misc.cpp
@@ -13,86 +13,148 @@ using namespace std;
 using namespace node;
 using namespace v8;
 
-static Handle<Value> pollpri(const Arguments&);
-static int pollpri_thread(eio_req *);
-static int pollpri_after(eio_req *);
-//extern "C" void pollpri_event(struct ev_loop* loop, ev_io* req, int revents);
 extern "C" void pollpri_event(ev_io* req, int revents);
 extern "C" void init(Handle<Object>);
 
-struct pollpri_request {
-    Persistent<Function> callback;
-    char path[1];
-};
+class Pollpri: ObjectWrap {
+private:
+    int fd, epfd;
+    char *path;
+public:
+    struct pollpri_request {
+        Pollpri *p;
+        Persistent<Function> cb;
+    };
 
-class Pollpri : public ObjectWrap {
-};
-
-static Handle<Value> pollpri(const Arguments& args) {
-    HandleScope scope;
-    const char *usage = "usage: pollpri(path, cb)";
-    if(args.Length() != 2) {
-        return ThrowException(Exception::Error(String::New(usage)));
-    }
-    String::Utf8Value path(args[0]);
-    Local<Function> cb = Local<Function>::Cast(args[1]);
+    static Persistent<FunctionTemplate> ct;
     
-    pollpri_request *pr = (pollpri_request *)
-        malloc(sizeof(struct pollpri_request) + path.length() + 1);
+    static void Init(Handle<Object> target) {
+        HandleScope scope;
+        Local<FunctionTemplate> t = FunctionTemplate::New(New);
+        ct = Persistent<FunctionTemplate>::New(t);
+        ct->InstanceTemplate()->SetInternalFieldCount(1);
+        ct->SetClassName(String::NewSymbol("Pollpri"));
+    
+        NODE_SET_PROTOTYPE_METHOD(ct, "pollpri", pollpri);
         
-    pr->cb = Persistent<Function>::New(cb);
-    strncpy(pr->path, *path, path.length() + 1);
+        target->Set(String::NewSymbol("Pollpri"), ct->GetFunction());
+    }
+
+    Pollpri() {
+        epfd = 0;
+        fd = 0;
+    }
     
-    eio_custom(pollpri_thread, EIO_PRI_DEFAULT, pollpri_after, pr);
-    ev_ref(EV_DEFAULT_UC);
-    printf("Leaving pollpri\n");
-    return(Undefined());
-}
+    ~Pollpri() {
+        if(epfd) close(epfd);
+        if(fd) close(fd);
+    }
+    
+    static Handle<Value> New(const Arguments &args) {
+        printf("Entered New\n");
+        HandleScope scope;
+        const char *usage = "usage: new Pollpri(path)";
+        if(args.Length() != 1) {
+            return ThrowException(Exception::Error(String::New(usage)));
+        }
+        String::Utf8Value path(args[0]);
+        Pollpri *p = new Pollpri();
+        p->Wrap(args.This());
+        p->path = (char *)malloc(path.length() + 1);
+        strncpy(p->path, *path, path.length() + 1);
+        printf("Leaving New\n");
+        return(args.This());
+    }
 
-static int pollpri_thread(eio_req *req) {
-    printf("Entered pollpri_thread\n");
-    struct pollpri_request * pr = (struct pollpri_request *)req->data;
-    int epfd = epoll_create(1);
-    int fd = open(pr->path, O_RDWR | O_NONBLOCK);
-    printf("open(%s) returned %d: %s\n", pr->path, fd, strerror(errno));
-    struct epoll_event ev;
-    ev.events = EPOLLPRI;
-    ev.data.fd = fd;
-    int n = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
-    printf("epoll_ctl(%d) returned %d (%d): %s\n", fd, n, epfd, strerror(errno));
-    ev_io pollpri_watcher;
-    ev_init(&pollpri_watcher, pollpri_event);
-    pollpri_watcher.data = pr;
-    ev_io_set(&pollpri_watcher, epfd, EV_READ | EV_WRITE);
-    ev_io_start(EV_DEFAULT_ &pollpri_watcher);
-    //struct epoll_event events;
-    //n = epoll_wait(epfd, &events, 1, -1);
-    //printf("epoll_wait(%d) returned %d: %s\n", epfd, n, strerror(errno));
-    printf("Leaving pollpri_thread\n");
-    return(0);
-}
+    static Handle<Value> pollpri(const Arguments& args) {
+        printf("Entered pollpri\n");
+        HandleScope scope;
+        const char *usage = "usage: pollpri(cb)";
+        if(args.Length() != 1) {
+            return ThrowException(Exception::Error(String::New(usage)));
+        }
+        Local<Function> cb = Local<Function>::Cast(args[0]);
+        
+        pollpri_request* pr = 
+            (pollpri_request *)malloc(sizeof(struct pollpri_request));
+        Pollpri* p = ObjectWrap::Unwrap<Pollpri>(args.This());
+        
+        pr->p = p;
+        pr->cb = Persistent<Function>::New(cb);
+        
+        eio_custom(pollpri_thread, EIO_PRI_DEFAULT, pollpri_after, pr);
+        ev_ref(EV_DEFAULT_UC);
+        printf("Leaving pollpri\n");
+        return(Undefined());
+    }
+    
+    static int pollpri_thread(eio_req *req) {
+        printf("Entered pollpri_thread\n");
+        struct pollpri_request * pr = (struct pollpri_request *)req->data;
+        Pollpri *p = pr->p;
+        int epfd = p->epfd;
+        int fd = p->fd;
+        if(!fd) {
+            fd = open(p->path, O_RDWR | O_NONBLOCK);
+            pr->p->fd = fd;
+            printf("open(%s) returned %d: %s\n", p->path, fd, strerror(errno));
+        }
+        if(!epfd) {
+            epfd = epoll_create(1);
+            pr->p->epfd = epfd;
+            printf("epoll_create(1) returned %d: %s\n", epfd, strerror(errno));
+            struct epoll_event ev;
+            ev.events = EPOLLPRI;
+            ev.data.fd = fd;
+            int n = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
+            printf("epoll_ctl(%d) returned %d (%d): %s\n", fd, n, epfd, strerror(errno));
+        }
+        //ev_io pollpri_watcher;
+        //ev_init(&pollpri_watcher, pollpri_event);
+        //pollpri_watcher.data = pr;
+        //ev_io_set(&pollpri_watcher, epfd, EV_READ | EV_WRITE);
+        //ev_io_start(EV_DEFAULT_ &pollpri_watcher);
+        struct epoll_event events;
+        printf("Calling epoll_wait\n");
+        sleep(1);
+        int m = epoll_wait(epfd, &events, 1, -1);
+        printf("epoll_wait(%d) returned %d: %s\n", epfd, m, strerror(errno));
+        //close(epfd);
+        //close(fd);
+        printf("Leaving pollpri_thread\n");
+        return(0);
+    }
+    
+    static int pollpri_after(eio_req *req) {
+        printf("Entered pollpri_after\n");
+        HandleScope scope;
+        ev_unref(EV_DEFAULT_UC);
+        struct pollpri_request * pr = (struct pollpri_request *)req->data;
+        Local<Value> argv[2];
+        argv[0] = Local<Value>::New(Null());
+        argv[1] = String::New(pr->p->path);
+        pr->cb->Call(Context::GetCurrent()->Global(), 2, argv);
+        pr->cb.Dispose();
+        //close(pr->p->epfd);
+        //close(pr->p->fd);
+        free(pr);
+        printf("Leaving pollpri_after\n");
+        return(0);
+    }
+    
+};
 
-extern "C" void pollpri_event(ev_io* req, int revents) {
-    printf("Entered pollpri_event\n");
-    printf("Leaving pollpri_event\n");
-}
+Persistent<FunctionTemplate> Pollpri::ct;
 
-static int pollpri_after(eio_req *req) {
-    printf("Entered pollpri_after\n");
-    HandleScope scope;
-    //ev_unref(EV_DEFAULT_UC);
-    struct pollpri_request * pr = (struct pollpri_request *)req->data;
-    Local<Value> argv[2];
-    argv[0] = Local<Value>::New(Null());
-    argv[1] = String::New(pr->path);
-    pr->cb->Call(Context::GetCurrent()->Global(), 2, argv);
-    //pr->cb.Dispose();
-    //free(pr);
-    printf("Leaving pollpri_after\n");
-    return(0);
-}
-
-extern "C" void init(Handle<Object> target) {
-    HandleScope scope;
-    NODE_SET_METHOD(target, "pollpri", pollpri);
+extern "C" {
+    void init(Handle<Object> target) {
+        Pollpri::Init(target);
+    }
+    
+    void pollpri_event(ev_io* req, int revents) {
+        printf("Entered pollpri_event\n");
+        printf("Leaving pollpri_event\n");
+    }
+    
+    NODE_MODULE(pollpri, init);
 }
--- bonescript/misctest.js
+++ bonescript/misctest.js
@@ -1,9 +1,11 @@
-var misc = require('./misc');
 var fs = require('fs');
-
 var filename = '/sys/class/gpio/gpio34/value';
+var misc = require('./misc');
+var pollpri = new misc.Pollpri(filename);
 
-console.log('Calling pollpri');
-misc.pollpri(filename, function(err, data) {
-    console.log(data + ': ' + fs.readFileSync(filename, 'utf-8'));
-});
+var readdata = function(err, data) {
+    console.log(data + ': ' + fs.readFileSync(data, 'utf-8'));
+    pollpri.pollpri(readdata);
+};
+
+pollpri.pollpri(readdata);
