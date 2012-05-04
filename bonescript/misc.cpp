e0cf403af28c1d9fdbf14bb2f676d392c1f242f6
Jason Kridner <jdk@ti.com>
2012-05-04 17:25:20 +0100

    Saving off temporary workspace
    misctest.js will currently immediately read the file without waiting.
    I'm looking to add an EventEmitter next.

--- bonescript/misc.cpp
+++ bonescript/misc.cpp
@@ -13,18 +13,22 @@ using namespace std;
 using namespace node;
 using namespace v8;
 
-static Handle<Value> PollpriAsync(const Arguments&);
-static int Pollpri(eio_req *);
-//static int PollpriAfter(eio_req *);
-static void PollpriAfter(ev_io* req, int revents);
+static Handle<Value> pollpri(const Arguments&);
+static int pollpri_thread(eio_req *);
+static int pollpri_after(eio_req *);
+//extern "C" void pollpri_event(struct ev_loop* loop, ev_io* req, int revents);
+extern "C" void pollpri_event(ev_io* req, int revents);
 extern "C" void init(Handle<Object>);
 
 struct pollpri_request {
-    Persistent<Function> cb;
+    Persistent<Function> callback;
     char path[1];
 };
 
-static Handle<Value> PollpriAsync(const Arguments& args) {
+class Pollpri : public ObjectWrap {
+};
+
+static Handle<Value> pollpri(const Arguments& args) {
     HandleScope scope;
     const char *usage = "usage: pollpri(path, cb)";
     if(args.Length() != 2) {
@@ -39,36 +43,42 @@ static Handle<Value> PollpriAsync(const Arguments& args) {
     pr->cb = Persistent<Function>::New(cb);
     strncpy(pr->path, *path, path.length() + 1);
     
+    eio_custom(pollpri_thread, EIO_PRI_DEFAULT, pollpri_after, pr);
+    ev_ref(EV_DEFAULT_UC);
+    printf("Leaving pollpri\n");
+    return(Undefined());
+}
+
+static int pollpri_thread(eio_req *req) {
+    printf("Entered pollpri_thread\n");
+    struct pollpri_request * pr = (struct pollpri_request *)req->data;
     int epfd = epoll_create(1);
     int fd = open(pr->path, O_RDWR | O_NONBLOCK);
     printf("open(%s) returned %d: %s\n", pr->path, fd, strerror(errno));
     struct epoll_event ev;
-    struct epoll_event events;
     ev.events = EPOLLPRI;
     ev.data.fd = fd;
     int n = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
     printf("epoll_ctl(%d) returned %d (%d): %s\n", fd, n, epfd, strerror(errno));
     ev_io pollpri_watcher;
-    ev_init(&pollpri_watcher, PollpriAfter);
+    ev_init(&pollpri_watcher, pollpri_event);
     pollpri_watcher.data = pr;
-    ev_io_set(&pollpri_watcher, epfd, EV_READ);
+    ev_io_set(&pollpri_watcher, epfd, EV_READ | EV_WRITE);
     ev_io_start(EV_DEFAULT_ &pollpri_watcher);
-
-    //eio_custom(Pollpri, EIO_PRI_DEFAULT, PollpriAfter, pr);
-    //ev_ref(EV_DEFAULT_UC);
-    printf("Leaving PollpriAsync\n");
-    return(Undefined());
-}
-
-static int Pollpri(eio_req *req) {
-    struct pollpri_request * pr = (struct pollpri_request *)req->data;
-    printf("Entered Pollpri\n");
+    //struct epoll_event events;
+    //n = epoll_wait(epfd, &events, 1, -1);
+    //printf("epoll_wait(%d) returned %d: %s\n", epfd, n, strerror(errno));
+    printf("Leaving pollpri_thread\n");
     return(0);
 }
 
-//static int PollpriAfter(eio_req *req) {
-static void PollpriAfter(ev_io* req, int revents) {
-    printf("Entered PollpriAfter\n");
+extern "C" void pollpri_event(ev_io* req, int revents) {
+    printf("Entered pollpri_event\n");
+    printf("Leaving pollpri_event\n");
+}
+
+static int pollpri_after(eio_req *req) {
+    printf("Entered pollpri_after\n");
     HandleScope scope;
     //ev_unref(EV_DEFAULT_UC);
     struct pollpri_request * pr = (struct pollpri_request *)req->data;
@@ -78,9 +88,11 @@ static void PollpriAfter(ev_io* req, int revents) {
     pr->cb->Call(Context::GetCurrent()->Global(), 2, argv);
     //pr->cb.Dispose();
     //free(pr);
+    printf("Leaving pollpri_after\n");
+    return(0);
 }
 
 extern "C" void init(Handle<Object> target) {
     HandleScope scope;
-    NODE_SET_METHOD(target, "pollpri", PollpriAsync);
+    NODE_SET_METHOD(target, "pollpri", pollpri);
 }
--- bonescript/misctest.js
+++ bonescript/misctest.js
@@ -1,6 +1,9 @@
 var misc = require('./misc');
+var fs = require('fs');
+
+var filename = '/sys/class/gpio/gpio34/value';
 
 console.log('Calling pollpri');
-misc.pollpri('/sys/class/gpio/gpio34/value', function(err, data) {
-    console.log(''+data);
+misc.pollpri(filename, function(err, data) {
+    console.log(data + ': ' + fs.readFileSync(filename, 'utf-8'));
 });
