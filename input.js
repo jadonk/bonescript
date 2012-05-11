cf72e6b531b16983b525ac6cf1381c61fc3a2e7e
Jason Kridner <jdk@ti.com>
2012-05-11 01:33:49 +0100

    input.js: reordered code

--- input.js
+++ input.js
@@ -4,7 +4,15 @@ var outputPin = bone.P8_3;
 var inputPin = bone.P8_5;
 
 var timeEvent = 0;
-
+var nextState = LOW;
+var toggleGPIO = function() {
+    console.log('Setting ' + outputPin.key + 
+        ((nextState == HIGH) ? ' HIGH' : ' LOW'));
+    timeEvent++;
+    console.time(''+timeEvent);
+    digitalWrite(outputPin, nextState);
+    nextState = (nextState == HIGH) ? LOW : HIGH;
+};
 var handler = function(pin, value) {
     console.log(pin.key + ' changed to ' + ((value == HIGH) ? 'HIGH' : 'LOW'));
     console.timeEnd(''+timeEvent);
@@ -16,19 +24,8 @@ setup = function() {
     pinMode(inputPin, INPUT);
     pinMode(outputPin, OUTPUT);
     attachInterrupt(inputPin, handler, CHANGE);
+    setInterval(toggleGPIO, 500);
 };
-
-var nextState = LOW;
-var toggleGPIO = function() {
-    console.log('Setting ' + outputPin.key + 
-        ((nextState == HIGH) ? ' HIGH' : ' LOW'));
-    timeEvent++;
-    console.time(''+timeEvent);
-    digitalWrite(outputPin, nextState);
-    nextState = (nextState == HIGH) ? LOW : HIGH;
-};
-
-setInterval(toggleGPIO, 500);
 
 loop = function() {
 };
