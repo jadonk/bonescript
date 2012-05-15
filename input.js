386154500b4f77eb3fd9de94d4529285577533fb
Jason Kridner <jkridner@beagleboard.org>
2012-05-15 22:39:12 +0000

    input.js: cleanup

--- input.js
+++ input.js
@@ -6,8 +6,6 @@ var inputPin = bone.P8_5;
 var timeEvent = 0;
 var nextState = HIGH;
 var toggleGPIO = function() {
-    console.log('Setting ' + outputPin.key + 
-        ((nextState == HIGH) ? ' HIGH' : ' LOW'));
     timeEvent++;
     console.time(''+timeEvent);
     digitalWrite(outputPin, nextState);
@@ -17,6 +15,15 @@ var handler = function(pin, value) {
     console.timeEnd(''+timeEvent);
     console.log(pin.key + ' changed to ' + ((value == HIGH) ? 'HIGH' : 'LOW'));
 };
+var startToggle = function() {
+    // The loop() function would block the running of the event handler.
+    // setInterval() is a way to get a function to run at an interval that
+    // does not block while it is waiting.  The challenge is you must provide
+    // a function that can be called, rather than just continue where you 
+    // left off.  This can add some complexity for beginners, but it has some
+    // benefits for writing embedded software.
+    setInterval(toggleGPIO, 500);
+};
 
 setup = function() {
     console.log('Please connect ' + inputPin.key + ' to ' + outputPin.key +
@@ -25,12 +32,7 @@ setup = function() {
     pinMode(outputPin, OUTPUT);
     digitalWrite(outputPin, LOW);
     attachInterrupt(inputPin, handler, CHANGE);
-    setInterval(toggleGPIO, 500);
+    setTimeout(startToggle, 500);  // give some time for the handler to spawn
 };
-
-//loop = function() {
-//    toggleGPIO();
-//    delay(500);
-//};
 
 bb.run();
