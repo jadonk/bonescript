1a9ef108ba247c1b5a39b7ec1e3b38f4cbcc672e
Koen Kooi <koen@dominion.thruhere.net>
2012-07-19 15:52:00 +0200

    weatherstation: modify to use weatherstation cape sensors
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- weatherstation.js
+++ weatherstation.js
@@ -2,8 +2,8 @@
 var bb = require('bonescript');
 var fs = require('fs');
 var io = require('socket.io');
-var pconfig = require('./weatherstation/bmp085-pressure');
-
+var bmp085config = require('./weatherstation/bmp085-pressure');
+var sht21config = require('./weatherstation/sht21-humidity');
 
 setup = function() {
     try {
@@ -11,11 +11,21 @@ setup = function() {
     } catch(ex) {
         console.log('bmp085 driver load failed.');
     }
+    try {
+        fs.writeFileSync('/sys/class/i2c-adapter/i2c-3/new_device', 'sht21 0x40', encoding='ascii');
+    } catch(ex) {
+        console.log('sht21 driver load failed.');
+    }
+    try {
+        fs.writeFileSync('/sys/class/i2c-adapter/i2c-3/new_device', 'tsl2550 0x39', encoding='ascii');
+    } catch(ex) {
+        console.log('tsl2550 driver load failed.');
+    }
     var onconnect = function(socket) {
         console.log("New client connected");
-        var pdelay = pconfig.pressureConfig.delay;
-        var pscale = pconfig.pressureConfig.scale;
-        var pfileData = pconfig.pressureConfig.file;
+        var pdelay = bmp085config.pressureConfig.delay;
+        var pscale = bmp085config.pressureConfig.scale;
+        var pfileData = bmp085config.pressureConfig.file;
 
         var preadData = function(fd) {
             fs.readFile(pfileData, function(err, data) {
@@ -25,9 +35,9 @@ setup = function() {
             setTimeout(preadData, pdelay);
         };
 
-        var tdelay = pconfig.tempConfig.delay;
-        var tscale = pconfig.tempConfig.scale;
-        var tfileData = pconfig.tempConfig.file;
+        var tdelay = bmp085config.tempConfig.delay;
+        var tscale = bmp085config.tempConfig.scale;
+        var tfileData = bmp085config.tempConfig.file;
         
         var treadData = function(fd) {
             fs.readFile(tfileData, function(err, data) {
@@ -37,10 +47,24 @@ setup = function() {
             setTimeout(treadData, tdelay);
         };
 
-        socket.emit('pressureconfig', pconfig.pressureConfig);
-        socket.emit('tempconfig', pconfig.tempConfig);
+        var hdelay = sht21config.humidityConfig.delay;
+        var hscale = sht21config.humidityConfig.scale;
+        var hfileData = sht21config.humidityConfig.file;
+        
+        var hreadData = function(fd) {
+            fs.readFile(hfileData, function(err, data) {
+                if(err) throw("Unable to read data: " + err);
+                socket.emit('humiditydata', "" + data / hscale);
+            });
+            setTimeout(hreadData, hdelay);
+        };
+
+        socket.emit('pressureconfig', bmp085config.pressureConfig);
+        socket.emit('tempconfig', bmp085config.tempConfig);
+        socket.emit('humidityconfig', sht21config.humidityConfig);
         setTimeout(treadData, tdelay);
         setTimeout(preadData, pdelay);
+        setTimeout(hreadData, hdelay);
 
         // on message
         socket.on('message', function(data) {
