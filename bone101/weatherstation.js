6f8239d8d95826567d961681d587071bd8e497c7
Jason Kridner <jdk@ti.com>
2012-11-29 19:41:08 +0100

    bone101/weaterstation.js: Updates from Koen

--- bone101/weatherstation.js
+++ bone101/weatherstation.js
@@ -1,16 +1,29 @@
 // Configuration data
 pressureConfig = {
+    file: "/sys/bus/i2c/drivers/bmp085/3-0077/pressure0_input",
     unit: "milliBar",
+    delay: 5000,
     scale: 100,
     rangeHigh: 1100,
     rangeLow: 900,
 };
 
 tempConfig = {
-    unit: "C",
+    file: "/sys/bus/i2c/drivers/bmp085/3-0077/temp0_input",
+    unit: "° F",
+    delay: 2000,
     scale: 10,
     rangeHigh: 40,
     rangeLow: -20,
+};
+
+humidityConfig = {
+    file: "/sys/bus/i2c/drivers/sht21/3-0040/humidity1_input",
+    unit: "% RH",
+    delay: 2000,
+    scale: 1000,
+    rangeHigh: 100,
+    rangeLow: 0,
 };
 
 // global vars
@@ -29,16 +42,38 @@ var tmax = tempConfig.rangeHigh;
 var tmin = tempConfig.rangeLow;
 var tunit = tempConfig.unit;
 
-canvasWidth = window.innerWidth * 0.9;
-//if ( canvasWidth > window.innerHeight) 
-// canvasWidth = 1.3 * window.innerHeight;
+var humidity = 50;
+var hmax = humidityConfig.rangeHigh;
+var hmin = humidityConfig.rangeLow;
+var hunit = humidityConfig.unit;
+
+var lux = 0;
+var lmax = 2000;
+var lmin = 0;
+var lunit = "lux";
+
+
+var setWidth = function() {
+ var canvasWidth = window.innerWidth * 0.9;
+ if ( canvasWidth > (1.8 * window.innerHeight)) {
+  canvasWidth = 1.8 * window.innerHeight;
+  $('#heading').hide();
+ } else
+  $('#heading').show();
+
+ barometerWidth = canvasWidth*0.5;
+ barometerHeight = barometerWidth;
+ thermometerWidth = canvasWidth*0.25;
+ lightWidth = canvasWidth*0.25;
+};
+setWidth();
 
 var barometerSketchProc = function(p) {
- p.size(canvasWidth*(2/3), canvasWidth*(2/3));
+ p.size(barometerWidth, barometerWidth);
  p.draw = function() {
-  p.size(canvasWidth*(2/3), canvasWidth*(2/3));
-  barometerWidth=p.width;
-  p.background(0,0);
+ p.size(barometerWidth, barometerWidth);
+ barometerWidth=p.width;
+ p.background(0,0);
   
   // barometer
   p.fill(220);
@@ -74,20 +109,56 @@ var barometerSketchProc = function(p) {
          p.text(myText, Math.round(barometerWidth/2 + Math.cos(angle) * barometerWidth*0.3 - myWidth/2),Math.round(barometerWidth/2 + Math.sin(angle) * barometerWidth*0.3));
        }
    }
-
-  //needle
+   
+  
+   // RH scale
+   p.fill(220);
+   p.stroke(0);
+   p.strokeWeight(barometerWidth*0.005);
+   p.arc(barometerWidth/2, barometerWidth/2, barometerWidth*0.4, barometerWidth*0.4, -(4/3)*PI, PI/3);
+   
+   // we want a range from 0 - 100%
+   // 1-5=60-100, 7-12=0-50
+   p.textSize(Math.round(barometerWidth*0.02));
+   for ( i = 1; i <= 12; i++, angle += inc ) {
+       if(i!=6) {
+         p.line(barometerWidth/2 + Math.cos(angle) * barometerWidth*0.18,barometerWidth/2 + Math.sin(angle) * barometerWidth*.18,barometerWidth/2 + Math.cos(angle) * barometerWidth*0.2,barometerWidth/2 + Math.sin(angle) * barometerWidth*.2);
+         if (i < 6) {
+           myText = 50 +10*i;
+         } else {
+           myText = 10*i - 70;
+         }     
+         myWidth = p.textWidth(myText);
+         p.fill(0, 102, 153);
+         p.text(myText, Math.round(barometerWidth/2 + Math.cos(angle) * barometerWidth*0.16 - myWidth/2),Math.round(barometerWidth/2 + Math.sin(angle) * barometerWidth*0.16));
+       } else {
+         myText = hunit;  
+         myWidth = p.textWidth(myText);
+         p.fill(0, 102, 153);
+         p.text(myText, Math.round(barometerWidth/2 + Math.cos(angle) * barometerWidth*0.12 - myWidth/2),Math.round(barometerWidth/2 + Math.sin(angle) * barometerWidth*0.12));
+       }
+   }
+   
+        //humidity needle
+  p.stroke(60);
+  p.strokeWeight(barometerWidth*0.005);
+  p.line(-Math.cos(humidity) * barometerWidth*0.05  + barometerWidth/2, -Math.sin(humidity) * barometerWidth*0.05 + barometerWidth/2, Math.cos(humidity) * barometerWidth*0.15 + barometerWidth/2, Math.sin(humidity) * barometerWidth*0.15 + barometerWidth/2);
+  //p.ellipse(barometerWidth/2, barometerWidth/2, barometerWidth/20, barometerWidth/20);
+   
+     // pressure needle
   p.stroke(60);
   p.strokeWeight(barometerWidth*0.015);
   p.line(-Math.cos(pressure) * barometerWidth*0.05  + barometerWidth/2, -Math.sin(pressure) * barometerWidth*0.05 + barometerWidth/2, Math.cos(pressure) * barometerWidth*0.35 + barometerWidth/2, Math.sin(pressure) * barometerWidth*0.35 + barometerWidth/2);
   p.ellipse(barometerWidth/2, barometerWidth/2, barometerWidth/20, barometerWidth/20);
+  
  };
  p.noLoop();
 }
 
 var thermometerSketchProc = function(p) {
- p.size(canvasWidth*(1/3), canvasWidth*(2/3));
+ p.size(thermometerWidth, barometerHeight);
  p.draw = function() {
-  p.size(canvasWidth*(1/3), canvasWidth*(2/3));
+  p.size(thermometerWidth, barometerHeight);
   thermometerWidth=p.width;
   p.background(0,0);
 
@@ -135,7 +206,7 @@ var thermometerSketchProc = function(p) {
     }    
     p.line((thermometerWidth/2) - thermometerWidth/8,theight,(thermometerWidth/2) - thermometerWidth/8 + extra, theight );
     
-    myText = -30 + i*10;
+    myText = (-30 + i*10)*1.8 + 32;
     p.fill(0, 0, 0);
     p.text(myText, (thermometerWidth/2) - thermometerWidth*0.09 + extra, theight + 4);
     
@@ -145,7 +216,7 @@ var thermometerSketchProc = function(p) {
     p.line((thermometerWidth/2) + thermometerWidth/8,thermometerWidth*1.1 - (thermometerWidth/50) * tmax,(thermometerWidth/2) + thermometerWidth/8 - thermometerWidth/20, thermometerWidth*1.1 - (thermometerWidth/50) * tmax );
     p.strokeWeight(thermometerWidth*0.03);
 }
-  myText = temp + tunit;
+  myText = Math.round((temp*1.8 + 32)*10)/10 + tunit;
   p.fill(0, 0, 0);
   p.textSize(Math.round(thermometerWidth*0.09));
   myWidth = p.textWidth(myText);
@@ -154,10 +225,37 @@ var thermometerSketchProc = function(p) {
  p.noLoop();
 }
 
-var barometerCanvas = document.getElementById("barometerCanvas");
+
+var lightSketchProc = function(p) {
+ p.size(lightWidth, barometerHeight);
+ p.draw = function() {
+  p.size(lightWidth, barometerHeight);
+  lightWidth=p.width;
+  p.background(0,0);
+
+  // contour
+  p.stroke(0);
+  p.strokeWeight(lightWidth*0.01);
+  if(lux > (3*255))
+  	lux = (3*255 - 10);
+  p.fill(lux/3 + 10);
+  p.ellipse(lightWidth/2,lightWidth,lightWidth/2,lightWidth/2);
+  
+  myText = lux + lunit;
+  p.fill(0, 0, 0);
+  p.textSize(Math.round(lightWidth*0.09));
+  myWidth = p.textWidth(myText);
+  p.text(myText, lightWidth/2 - myWidth/2, lightWidth*1.4 + lightWidth*0.045);
+ };
+ p.noLoop();
+}
+
+var canvas = document.getElementById("barometerCanvas");
 var thermometerCanvas = document.getElementById("thermometerCanvas");
-var barometer = new Processing(barometerCanvas, barometerSketchProc);
+var lightCanvas = document.getElementById("lightCanvas");
+var barometer = new Processing(canvas, barometerSketchProc);
 var thermometer = new Processing(thermometerCanvas, thermometerSketchProc);
+//var light = new Processing(lightCanvas, lightSketchProc);
 
 pressureUpdate = function(data) {
  var myData = parseFloat(data) / pressureConfig.scale;
@@ -172,27 +270,37 @@ pressureUpdate = function(data) {
  barometer.redraw();
 };
 
-temperatureUpdate = function(data) {
- var myData = parseFloat(data) / tempConfig.scale;
+humidityUpdate = function(data) {
+ var myData = parseFloat(data) / humidityConfig.scale;
  // Angles for sin() and cos() start at 3 o'clock;
  // subtract HALF_PI to make them start at the top
+ // 30% = HALF_PI
+ humidity = (myData  - 50) * (TWO_PI / 120) - HALF_PI;
+
+ if (myData > hmax) hmax = myData;
+ if (myData < hmin) hmin = myData;
+ 
+ barometer.redraw();
+};
+
+temperatureUpdate = function(data) {
+ var myData = parseFloat(data) / tempConfig.scale;
  temp = myData;
  if (myData > tmax) tmax = myData;
  if (myData < tmin) tmin = myData;
  thermometer.redraw();
 };
 
+luxUpdate = function(data) {
+ lux = data;
+ light.redraw();
+ document.getElementById("light").innerHTML=lux + " lux"; 
+};
+
 window.onresize=resizeHandler;
 function resizeHandler(){
- canvasWidth = window.innerWidth * 0.9;
- if ( canvasWidth > window.innerHeight) 
-  canvasWidth = 1.3 * window.innerHeight;
- 
- barometerCanvas.width = canvasWidth*(2/3);
- barometerCanvas.height = canvasWidth*(2/3);
+ setWidth();
  barometer.redraw();
- 
- thermometerCanvas.width = canvasWidth*(1/3);
- thermometerCanvas.height = canvasWidth*(2/3);
  thermometer.redraw();
+ light.redraw();
 }
