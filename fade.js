0727c5960d9a0bd15744c7d27f6f325095086203
Jason Kridner <jdk@ti.com>
2013-04-01 19:28:54 -0400

    fade.js: remove undesired semicolon

--- fade.js
+++ fade.js
@@ -18,4 +18,4 @@ function fade() {
  awValue = awValue + (awDirection*0.01);
  if(awValue > 1.0) { awValue = 1.0; awDirection = -1; }
  else if(awValue <= 0.01) { awValue = 0.01; awDirection = 1; }
-};
+}
