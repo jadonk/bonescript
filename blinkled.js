af784f74754075d4ed6ae8a0075457b3de6c195b
Jason Kridner <jdk@ti.com>
2013-04-01 20:53:06 -0400

    blinkled.js: simplified

--- blinkled.js
+++ blinkled.js
@@ -1,31 +1,20 @@
-// Import functions for interacting with hardware
 var b = require('bonescript');
 
 var ledPin = "P8_13";
-var ledPin2 = "USR0";
+var ledPin2 = "USR3";
 
-// Turn off USR1, USR2 and USR3
-b.pinMode("USR1", b.OUTPUT);
-b.digitalWrite("USR1", b.LOW);
-b.pinMode("USR2", b.OUTPUT);
-b.digitalWrite("USR2", b.LOW);
-b.pinMode("USR3", b.OUTPUT);
-b.digitalWrite("USR3", b.LOW);
-
-// Initialize pins as outputs
 b.pinMode(ledPin, b.OUTPUT);
 b.pinMode(ledPin2, b.OUTPUT);
 
-// Assume LEDs are off at the start
 var state = b.LOW;
+b.digitalWrite(ledPin, state);
+b.digitalWrite(ledPin2, state);
 
-// Call function to toggle LEDs every 100ms
-setInterval(toggleLEDs, 100);
+setInterval(toggle, 1000);
 
-// Define function to change the state of the LEDs
-function toggleLEDs() {
-    if(state == b.HIGH) state = b.LOW;
-    else state = b.HIGH;
+function toggle() {
+    if(state == b.LOW) state = b.HIGH;
+    else state = b.LOW;
     b.digitalWrite(ledPin, state);
     b.digitalWrite(ledPin2, state);
-};
+}
