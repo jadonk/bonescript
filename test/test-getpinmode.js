62dd901ae573e2ade5579abbb069a5c07104b176
Jason Kridner <jdk@ti.com>
2013-12-10 20:00:14 -0500

    getPinMode: get GPIO status of pin configuration
    var x = getPinMode(pin);
    * x.gpio.active: boolean if kernel has GPIO exported
    * x.gpio.allocated: boolean if application has allocated GPIO
    * x.gpio.direction: direction of GPIO

--- test/test-getpinmode.js
+++ test/test-getpinmode.js
@@ -2,8 +2,14 @@ var b = require('bonescript');
 
 b.analogWrite('P9_14', 0.1);
 
+console.log(JSON.stringify(b.getPinMode('USR3')));
 console.log(JSON.stringify(b.getPinMode('P9_12')));
 console.log(JSON.stringify(b.getPinMode('P9_13')));
 console.log(JSON.stringify(b.getPinMode('P9_14')));
-console.log(JSON.stringify(b.getPinMode('P9_15')));
 console.log(JSON.stringify(b.getPinMode('P9_35')));
+
+console.log(JSON.stringify(b.getPinMode('P9_15')));
+b.writeTextFile('/sys/class/gpio/unexport', ''+b.bone.pins['P9_15'].gpio);
+console.log(JSON.stringify(b.getPinMode('P9_15')));
+b.pinMode('P9_15', b.OUTPUT);
+console.log(JSON.stringify(b.getPinMode('P9_15')));
