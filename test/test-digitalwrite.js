a1b1bd93345118cf00f414a59cbb86aeac1afb03
Jason Kridner <jdk@ti.com>
2014-02-11 13:04:13 -0500

    Tolerate some calls without callbacks provided
    Assume asynchronous is alright, despite not having a callback. This is a
    bit of a risky choice that might surprise some users, but everyone is
    going to need to be told about making synchronous calls within setup()
    and loop() anyway very proactively.

--- test/test-digitalwrite.js
+++ test/test-digitalwrite.js
@@ -1,13 +1,18 @@
 var b = require('bonescript');
 
-b.analogWrite('P9_14', 0.2);
-b.pinMode('P9_14', b.OUTPUT);
-b.digitalWrite('P9_14', false);
-b.digitalWrite('P9_14', true);
+b.pinMode('P9_16', b.ANALOG_OUTPUT);
 
-b.pinMode('P9_15', b.OUTPUT);
-console.log(JSON.stringify(b.getPinMode('P9_15')));
-b.digitalWrite('P9_15', false);
-console.log(JSON.stringify(b.getPinMode('P9_15')));
-b.digitalWrite('P9_15', true);
-console.log(JSON.stringify(b.getPinMode('P9_15')));
+global.setup = function() {
+    b.analogWrite('P9_14', 0.2);
+    b.pinMode('P9_14', b.OUTPUT);
+    b.digitalWrite('P9_14', false);
+    b.digitalWrite('P9_14', true);
+    
+    console.log(JSON.stringify(b.getPinMode('P9_16')));
+    b.pinMode('P9_16', b.OUTPUT);
+    console.log(JSON.stringify(b.getPinMode('P9_16')));
+    b.digitalWrite('P9_16', false);
+    console.log(JSON.stringify(b.getPinMode('P9_16')));
+    b.digitalWrite('P9_16', true);
+    console.log(JSON.stringify(b.getPinMode('P9_16')));
+};
