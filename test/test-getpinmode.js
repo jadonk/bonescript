d675494a4581f9223f5ac501839f37762c744080
Jason Kridner <jdk@ti.com>
2014-02-11 11:56:51 -0500

    Restored setup/loop and eliminated synchronous calls outside them
    The code to handle both synchronous and asynchronous calls was getting
    confusing, so I am eliminating all synchronous implementations and
    providing a solution for those who want synchronous calls exclusively
    within a node-fiber. To invoke a fiber, simply define setup/loop.
    
    Example:
    
    ````javascript
    var b = require('bonescript');
    
    global.setup = function() {
        b.pinMode('P9_14');
    };
    
    global.loop = function() {
        var x = 0;
        b.analogWrite('P9_14', x);
        x += 0.1;
        if(x >= 1.0) x = 0;
    };
    ````

--- test/test-getpinmode.js
+++ test/test-getpinmode.js
@@ -1,15 +1,41 @@
 var b = require('bonescript');
+var fs = require('fs');
+var my = require('../src/my.js');
 
-b.analogWrite('P9_14', 0.1);
+global.setup = function() {
+    var capemgr = my.is_capemgr();
+    var slots = fs.readFileSync(capemgr + '/slots', 'ascii');
+    console.log(slots);
+    var slot = slots.match(/^\s*\d+\s*:.*,bs.*P9_14/gm);
+    console.log(slot);
+    slot = slots.match(/\d+(?=\s*:.*,bs.*P9_14)/gm);
+    console.log(slot);
+    if(slot && slot[0]) {
+        fs.writeFileSync(capemgr + '/slots', '-'+slot[0], 'ascii');
+    }
+    slots = fs.readFileSync(capemgr + '/slots', 'ascii');
+    console.log(slots);
 
-console.log(JSON.stringify(b.getPinMode('USR3')));
-console.log(JSON.stringify(b.getPinMode('P9_12')));
-console.log(JSON.stringify(b.getPinMode('P9_13')));
-console.log(JSON.stringify(b.getPinMode('P9_14')));
-console.log(JSON.stringify(b.getPinMode('P9_35')));
-
-console.log(JSON.stringify(b.getPinMode('P9_15')));
-b.writeTextFile('/sys/class/gpio/unexport', ''+b.bone.pins.P9_15.gpio);
-console.log(JSON.stringify(b.getPinMode('P9_15')));
-b.pinMode('P9_15', b.OUTPUT);
-console.log(JSON.stringify(b.getPinMode('P9_15')));
+    deleteFile('/lib/firmware/bspwm_P9_14_e-00A0.dtbo');
+    deleteFile('/lib/firmware/bspwm_P9_14_6-00A0.dtbo');
+    
+    function deleteFile(filename) {
+        if(my.file_existsSync(filename)) {
+            fs.unlinkSync(filename);
+        }
+    }
+    
+    b.analogWrite('P9_14', 0.1);
+    
+    console.log(JSON.stringify(b.getPinMode('USR3')));
+    console.log(JSON.stringify(b.getPinMode('P9_12')));
+    console.log(JSON.stringify(b.getPinMode('P9_13')));
+    console.log(JSON.stringify(b.getPinMode('P9_14')));
+    console.log(JSON.stringify(b.getPinMode('P9_35')));
+    
+    console.log(JSON.stringify(b.getPinMode('P9_15')));
+    b.writeTextFile('/sys/class/gpio/unexport', ''+b.bone.pins.P9_15.gpio);
+    console.log(JSON.stringify(b.getPinMode('P9_15')));
+    b.pinMode('P9_15', b.OUTPUT);
+    console.log(JSON.stringify(b.getPinMode('P9_15')));
+};
