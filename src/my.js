083eaf6a299f798f8517207437a2c5a6c80e71c7
Aditya Patadia <aditya@patadia.org>
2014-05-02 12:37:21 +0530

    obonescript migrate.

--- src/my.js
+++ src/my.js
@@ -213,7 +213,7 @@ exports.create_dt = function(pin, data, template, load, force_create, resp, call
     }
 
     function createDTS() {
-        var templateFilename = require.resolve('bonescript').replace('index.js',
+        var templateFilename = require.resolve('obonescript').replace('index.js',
             'dts/' + template + '_template.dts');
         if(debug) winston.debug('Creating template: ' + templateFilename);
         var dts = fs.readFileSync(templateFilename, 'utf8');
