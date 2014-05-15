8d9c4c2bbfee9db479732eb8f8a0021fa8c87d76
Jason Kridner <jdk@ti.com>
2014-05-15 17:07:45 -0400

    server: attempt to change server event listners

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 var b = require('bonescript');
+var util = require('util');
 
-b.serverStart();
+var server = b.serverStart();
+
+server.on('server$listening', serverListening);
+server.on('server$error', serverError);
+server.on('server$connection', serverConnection);
+
+function serverListening() {
+    if(debug) winston.debug('Server listening');
+}
+
+function serverError(error) {
+    if(debug) winston.debug('Server error: ' + JSON.stringify(error));
+}
+
+function serverConnection(connection) {
+    if(debug) winston.debug('Server connection: ' + connection);
+}
+
