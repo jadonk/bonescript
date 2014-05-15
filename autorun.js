15be08a6050358266eeb34b94003f3bf8e946a14
Jason Kridner <jdk@ti.com>
2014-05-15 13:11:52 -0400

    server/autorun: separated again
    Tested via:
    jason@timbird ~/workspace/bonescript $ NODE_PATH=~/workspace/ PORT=8080 SERVER_DIR=~/workspace/bone101 node server.js
    jason@timbird ~/workspace/bonescript $ NODE_PATH=~/workspace/ PORT=8080 SERVER_DIR=~/workspace/bone101 npm start
    jason@timbird ~/workspace/bonescript $ NODE_PATH=~/workspace AUTORUN_DIR=~/workspace/autorun-test node autorun.js

--- autorun.js
+++ autorun.js
@@ -1,1 +1,5 @@
-console.log('now part of server.js');
+var b = require('bonescript');
+
+var directory = (process.env.AUTORUN_DIR) ? process.env.AUTORUN_DIR : '/var/lib/cloud9/autorun';
+
+b.autorun(directory);
