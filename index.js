0604c9cd1517ab6f446a85abdfd1b5c614c39690
Jason Kridner <jdk@ti.com>
2014-05-18 15:45:05 +0000

    Used create_dt to load universal cape
    * Now to be called bonescript_default

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var hw;
 if(os.type() == 'Linux' || os.arch() == 'arm') {
     if(my.is_capemgr()) {
         if(!my.is_cape_universal()) {
-            my.load_dt('cape-bonescript');
+            my.create_dt({"key":"default", "options":{}}, 0, "bonescript", true);
         }
         if(my.is_cape_universal()) {
             hw = hw_universal;
