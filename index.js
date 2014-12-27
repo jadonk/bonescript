34fa9a259116041dd0f51e302cbdb202be40ad15
Aditya Patadia <aditya@patadia.org>
2014-12-27 21:48:16 +0530

    removed old cape manager

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var hw = null;
 if(os.type() == 'Linux' && os.arch() == 'arm') {
     if(!bone.is_cape_universal()) {
         winston.debug('Loading Universal Cape interface...');
-        bone.create_dt_sync({"key":"d", "options":{}}, 0, "OBS_UNIV", true, true);
+        bone.create_dt_sync({"key":"d", "options":{}}, 0, "OBS_UNIV", true);
         if(!bone.is_audio_enable()){
             winston.debug('Loading AUDIO Cape...');
             bone.create_dt_sync({"key":"d", "options":{}}, 0, "OBS_AUDIO", true);
