c2bc109e0eb9fe0e711922a38219b903d9dcfba9
Jason Kridner <jdk@ti.com>
2015-05-20 08:51:20 -0400

    i2c: move address definition to port

--- src/iic.js
+++ src/iic.js
@@ -11,15 +11,15 @@ m.events = {
 };
 m.openPorts = {};
 m.doOpen = function(args) {
-    var path = args.port;
-    if(m.ports[args.port].path) path = m.ports[args.port].path;
+    var path = args.port[0];
+    if(m.ports[args.port[0]].path) path = m.ports[args.port[0]].path;
     if(typeof args.options !== typeof {}) args.options = {};
     args.options.device = path;
-    var openPort = new m.module(args.address, args.options);
+    var openPort = new m.module(args.port[1], args.options);
     return(openPort);
 };
 
-exports.i2cOpen = my.wrapOpen(m, ['address', 'options']);
+exports.i2cOpen = my.wrapOpen(m, ['options']);
 exports.i2cScan = my.wrapCall(m, 'scan', [], ['err', 'data']);
 exports.i2cWriteByte = my.wrapCall(m, 'writeByte', ['byte'], ['err']);
 exports.i2cWriteBytes = my.wrapCall(m, 'writeBytes', ['command', 'bytes'], ['err']);
