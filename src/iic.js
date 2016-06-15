655c1a2bf1140a8a6a73e274adac292f4e79dcde
Mat Taylor <mat.taylor@gmail.com>
2016-06-15 14:22:57 -0700

    Update iic.js

--- src/iic.js
+++ src/iic.js
@@ -15,14 +15,17 @@ m.doOpen = function(args) {
     if(m.ports[args.port[0]].path) path = m.ports[args.port[0]].path;
     if(typeof args.options !== typeof {}) args.options = {};
     args.options.device = path;
-    var openPort = new m.module(args.port[1], args.options);
-    return(openPort);
+    //var openPort = new m.module(args.port[1], args.options);
+    var devId = args.options.hwInterface ? args.options.device.substring(3) : 1
+    var i2cBus = m.module.open(devId)
+    i2cBus.address = args.port[1]
+    return(i2cBus);
 };
 
-exports.i2cOpen = my.wrapOpen(m, ['options']);
-exports.i2cScan = my.wrapCall(m, 'scan', [], ['err', 'data']);
-exports.i2cWriteByte = my.wrapCall(m, 'writeByte', ['byte'], ['err']);
-exports.i2cWriteBytes = my.wrapCall(m, 'writeBytes', ['command', 'bytes'], ['err']);
-exports.i2cReadByte = my.wrapCall(m, 'readByte', [], ['err', 'res']);
-exports.i2cReadBytes = my.wrapCall(m, 'readBytes', ['command', 'length'], ['err', 'res']);
-exports.i2cStream = my.wrapCall(m, 'stream', ['command', 'length', 'delay'], []);
+exports.i2cOpen = my.wrapOpen(m, m.address, ['options']);
+exports.i2cScan = my.wrapCall(m, m.address, 'scan', [], ['err', 'data']);
+exports.i2cWriteByte = my.wrapCall(m, m.address, 'writeByte', ['byte'], ['err']);
+exports.i2cWriteBytes = my.wrapCall(m, m.address, 'writeBytes', ['command', 'bytes'], ['err']);
+exports.i2cReadByte = my.wrapCall(m, m.address, 'readByte', [], ['err', 'res']);
+exports.i2cReadBytes = my.wrapCall(m, m.address, 'readBytes', ['command', 'length'], ['err', 'res']);
+exports.i2cStream = my.wrapCall(m, m.address, 'stream', ['command', 'length', 'delay'], []);
